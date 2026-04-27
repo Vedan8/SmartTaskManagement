f9be07725cb66d05a67e5194464400319ab966af
Vedan <vedanjyadav@gmail.com>
2026-04-27 14:42:20 +0530

    cors

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import dbConnect from "./config/dbConnect"
 import dotenv from "dotenv"
 import { userRoute } from "./routes/userRoutes";
 import { taskRoute } from "./routes/taskRoutes";
+import cors from "cors";
 
 dotenv.config();
 const PORT=process.env.PORT
@@ -12,7 +13,7 @@ async function start() {
   await dbConnect(URI);
 
   const app = express();
-
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(userRoute)
